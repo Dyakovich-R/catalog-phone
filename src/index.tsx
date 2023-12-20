58046a1d97693ab3b57ff3138d07441e200777d2
Roman <rdyakovich@gmail.com>
2023-12-20 18:42:23 +0200

    catalog

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+import { Provider } from 'react-redux';
+import { HashRouter as Router } from 'react-router-dom';
 import { App } from './App';
+import { store } from './helpers/store';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+const Root = () => (
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
 );
+
+ReactDOM.render(<Root />, document.getElementById('root'));
